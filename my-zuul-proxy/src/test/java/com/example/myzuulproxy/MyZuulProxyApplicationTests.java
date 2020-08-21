e1e87b03ebebe2b7c708977b4f87a904713fe5ad
Nilay <54245048+dnilay@users.noreply.github.com>
2020-08-21 14:07:50 +0530

    Add files via upload

---
+++ my-zuul-proxy/src/test/java/com/example/myzuulproxy/MyZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myzuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyZuulProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
